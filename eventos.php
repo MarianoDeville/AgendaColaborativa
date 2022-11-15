2dd2073ea0c2026152301474d6f6b6d04a0dc35c
MarianoDeville <mariano_deville@hotmail.com>
2022-11-15 13:02:56 -0300

    Andando
    Proyecto completo.

---
+++ eventos.php
@@ -0,0 +1,69 @@
+<?php
+
+    $host="localhost";
+    $bd="sharepoint";
+    $usuario="mycolabora";
+    //$usuario="root";
+    $contraseña="b0tMjbxz";
+    //$contraseña="";
+
+    header('Content-Type: application/json');
+    $pdo=new PDO("mysql:dbname=$bd;host=$host", $usuario, $contraseña);
+    $accion= (isset($_GET['accion']))?$_GET['accion']:'leer';
+
+    switch($accion){
+
+        case 'agregar':
+            $sentenciaSQL=$pdo->prepare("INSERT INTO 
+                                        eventos(title,descripcion,color,textColor,start,end) 
+                                        VALUES(:title,:descripcion,:color,:textColor,:start,:end)");
+
+            $respuesta=$sentenciaSQL->execute(array(
+                                        "title"=>$_POST['title'],
+                                        "descripcion"=>$_POST['descripcion'],
+                                        "color"=>$_POST['color'],
+                                        "textColor"=>$_POST['textColor'],
+                                        "start"=>$_POST['start'],
+                                        "end"=>$_POST['end']));
+            echo json_encode($respuesta);
+            break;
+
+        case 'eliminar':
+            $respuesta=false;
+            if(isset($_POST['id'])){
+
+                $sentenciaSQL=$pdo->prepare("DELETE FROM eventos WHERE id=:id");
+                $respuesta= $sentenciaSQL->execute(array("id"=>$_POST['id']));
+            }
+            echo json_encode($respuesta);
+            break;
+
+        case 'modificar':
+            $sentenciaSQL=$pdo->prepare("UPDATE eventos SET 
+                                        title=:title, 
+                                        descripcion=:descripcion, 
+                                        color=:color, 
+                                        textColor=:textColor, 
+                                        start=:start, 
+                                        end=:end 
+                                        WHERE id=:id");
+            $respuesta=$sentenciaSQL->execute(array(
+                                        "title"=>$_POST['title'],
+                                        "descripcion"=>$_POST['descripcion'],
+                                        "color"=>$_POST['color'],
+                                        "textColor"=>$_POST['textColor'],
+                                        "start"=>$_POST['start'],
+                                        "end"=>$_POST['end'],
+                                        "id"=>$_POST['id']));
+            echo json_encode($respuesta);
+            break;
+
+        default:
+            $sentenciaSQL=$pdo->prepare("SELECT * FROM eventos");
+            $sentenciaSQL->execute();
+
+            $resultado=$sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
+            echo json_encode($resultado);
+            break;
+    }
+?>
